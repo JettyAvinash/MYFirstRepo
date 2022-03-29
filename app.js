6c6470ab973168e7a90ecdfc4dc58ffa1f31e3a8
Jetty Avinash <avinashjetty123@gmail.com>
2022-03-29 13:17:53 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,13 @@
+function sayHello(){
+    console.log("Hello Dude...How R U");
+}
+
+//console.log(module);
+
+//sayHello();
+
+var logObj = require('./logger');
+
+logObj('THIS is a mesg');
+
+//console.log(logObj);
---
+++ logger.js
@@ -0,0 +1,9 @@
+
+
+var url='http://mylogger.io/log';  //some silly URL name 
+
+function logFunc(message){
+    console.log(message);
+}
+
+module.exports= logFunc;
